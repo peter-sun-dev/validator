5b2b03ec824cf7769140ba4f278f1e448670a3ec
Peter <ugly0226@gmail.com>
2022-06-10 22:20:11 -0400

    update ini_validator.sh file

--- init_validator.sh
+++ init_validator.sh
@@ -6,20 +6,21 @@ echo
 
 # Install dependencies
 #sudo apt install wget
-wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.18.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
 source ~/.profile
 
 # Clone and build polygons-sdk
 
 echo "Building Go executable, please wait..."
-go install github.com/0xPolygon/polygon-edge@develop && cd go/bin/
-sudo mv polygon-edge /usr/local/bin && cd ~/
+
+go install github.com/0xPolygon/polygon-edge@develop && cd /home/$USER/go/bin/
+sudo mv polygon-edge /usr/local/bin
 
 # Initialize validator dir
 echo "Initializing validator directory.."
-rm -rf validator
-mkdir validator && cd validator/
+
+rm -rf data
 polygon-edge secrets init --data-dir data
 echo
